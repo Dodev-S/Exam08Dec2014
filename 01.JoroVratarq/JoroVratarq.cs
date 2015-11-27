a455a3815bea33b0c9e152b92081d5082b4dd662
Dodev-S <svetoslav.dodev@gmail.com>
2015-11-27 12:20:26 +0200

    no message

---
+++ 01.JoroVratarq/JoroVratarq.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.JoroVratarq
+{
+    class JoroVratarq
+    {
+        static void Main(string[] args)
+        {
+            string leap = Console.ReadLine();
+            int holidays = int.Parse(Console.ReadLine());
+            int hometown = int.Parse(Console.ReadLine());
+
+            double numberOfPlays = holidays / 2.0;
+            numberOfPlays += hometown;
+            numberOfPlays += ((52 - hometown) * (2.0 / 3));
+
+            if (leap == "t")
+            {
+                numberOfPlays += 3;
+            }
+
+            Console.WriteLine((int)numberOfPlays);
+        }
+    }
+}
---
+++ 02.HalfSum/HalfSum.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.HalfSum
+{
+    class HalfSum
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum1 = 0;
+            int sum2 = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                sum1 += int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < n; i++)
+            {
+                sum2 += int.Parse(Console.ReadLine());
+            }
+
+            if (sum2 == sum1)
+            {
+                Console.WriteLine("Yes, sum={0}", sum2);
+            }
+            else
+            {
+                Console.WriteLine("No, diff={0}", Math.Abs(sum2 - sum1));
+            }
+        }
+    }
+}
